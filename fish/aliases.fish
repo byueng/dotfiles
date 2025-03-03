bfa2e7a3dfe98d79ac1f2ba7c03730f02bbdff71
jwm <wzskqjh@gmail.com>
2025-03-03 14:34:31 +0800

    add some alias

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -2,6 +2,8 @@
 alias c="clear"
 alias n="neofetch"
 alias r="ranger"
+alias l="ls"
+alias s="source"
 
 alias font='source /home/byun/code/font_development/font/bin/activate.fish'
 alias test_venv='source /home/byun/code/test/test_venv/bin/activate.fish'
