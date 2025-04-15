fbb874bbba2ea3d67b0bb68dd002f356b5780aa1
jwm <wzskqjh@gmail.com>
2025-04-15 11:46:27 +0800

    2025年 4月15日 星期二 09时59分05秒 CST

---
+++ nvim/lua/plugins/editor/neo-tree.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  lazy = false,
+  opts = {
+    window = {
+      width = 25,
+      mappings = {},
+    },
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+      },
+    },
+  },
+}
