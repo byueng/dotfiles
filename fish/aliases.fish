56d6f2a3cb8c693358a9c23e5b1a3d4c31aa9712
jwm <wzskqjh@gmail.com>
2025-04-27 13:22:24 +0800

    2025年 4月27日 星期日 13时22分24秒 CST

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,6 +1,6 @@
 # all alias
 alias c="clear"
-alias n="neofetch" 
+alias n="neofetch"
 alias r="ranger"
 alias l="ls"
 alias e="exit"
@@ -11,3 +11,4 @@ alias python="python3"
 alias ff="fastfetch"
 alias confidence='source /Users/apple/code/NL2SQL/confidence/bin/activate.fish'
 alias car_venv='source /Users/apple/code/Car/car_backend/car_venv/bin/activate.fish'
+alias poker_venv='source /Users/apple/code/Simulating-Dreams/Agent-Poker/poker_venv/bin/activate.fish'
